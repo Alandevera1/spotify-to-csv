fd5752bb5531151ec3dd3ff151bc50fb8ad56567
Alandevera1 <alandevera1@vt.edu>
2021-07-27 20:56:53 -0400

    creating main method + documentation

--- spotify-to-csv.py
+++ spotify-to-csv.py
@@ -10,40 +10,12 @@ from dotenv import load_dotenv
 # QOL changes
 ### TODO ###
 
-'''Setting up'''
-load_dotenv()
-
-CLIENT_ID = os.getenv('CLIENT_ID')  # Spotify integration ID
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')  # Spotify integration secret
-
-# authorization URL to gain access to the Spotify API
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-# base URL of all Spotify API endpoints
-BASE_URL = 'https://api.spotify.com/v1/'
-
-# POST
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-    "expires_in": 3600
-})
-
-# convert the response to JSON
-auth_response_data = auth_response.json()
-
-# save the access token
-access_token = auth_response_data['access_token']
-
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
-
-
 def get_playlist_tracks(playlist_url):
     # Indices of the URI in the playlist link
     playlist_id = playlist_url[len(
         'https://open.spotify.com/playlist/'):len('https://open.spotify.com/playlist/') + 22]
+
+    ''' getting track uris '''
     playlist = requests.get(BASE_URL + 'playlists/' + playlist_id +
                             '/tracks', headers=headers)  # Gets the url and requests data
     playlist = playlist.json()
@@ -62,6 +34,7 @@ def get_album_tracks(album_url):
     album_id = album_url[len('https://open.spotify.com/album/')                         
                         :len('https://open.spotify.com/album/') + 22]
 
+    ''' getting track uris '''
     playlist = requests.get(BASE_URL + 'albums/' +
                             album_id + '/tracks', headers=headers)
     playlist = playlist.json()
@@ -73,7 +46,7 @@ def get_album_tracks(album_url):
     return get_music_data(playlist_track_uri)
 
 
-'''CREATING A DATAFRAME OF THE AUDIO FEATURES OF TRACKS'''
+''' CREATING A DATAFRAME OF THE AUDIO FEATURES OF TRACKS '''
 
 ''' 
                                 <COLUMNS>
@@ -90,6 +63,7 @@ def get_music_data(dict_uri):
 
     counter = 0
     for i in dict_uri:
+        ''' getting track information '''
         song_info = requests.get(BASE_URL + 'tracks/' + i, headers=headers)
         song_info = song_info.json()
 
@@ -105,6 +79,7 @@ def get_music_data(dict_uri):
 
         name = name.append(pd.DataFrame(data=data, index=[counter]))
 
+        ''' getting track audio features '''
         song_info = requests.get(
             BASE_URL + 'audio-features/' + i, headers=headers)
         song_info = song_info.json()
@@ -114,13 +89,14 @@ def get_music_data(dict_uri):
 
         counter += 1
 
+    ''' merging song information and audio features '''
     music_data = music_data.rename(columns={'track_href': 'href'})
     music_data = name.merge(music_data, on='href')
 
     return music_data
 
 
-'''Takes the dataframe and prints it to csv'''
+''' Takes the dataframe and prints it to csv '''
 
 
 def playlist_to_csv(playlist):
@@ -134,70 +110,116 @@ def playlist_to_csv(playlist):
         playlist.to_csv(user_answer + '.csv', index=False,
                         mode='a', header=False)
 
+     
+''' Running the script '''
+def run():
+    final_playlist = pd.DataFrame()
+    keepRunning = True
 
-'''Running the script'''
+    while (keepRunning is True):
+        user_answer = input(
+            'What type of playlist do you want to get data from?\n[playlist] for your own\n[album] for an album\n[track] for a single song\n[quit] to quit: ')
 
-final_playlist = pd.DataFrame()
-keepRunning = True
+        if (user_answer.lower() == 'album'):
+            user_answer = input('Input an album URL: ')
+            try: # Exception handling if the user inputs an invalid URL 
+                album_tracks = get_album_tracks(user_answer)
 
-while (keepRunning is True):
-    user_answer = input(
-        'What type of playlist do you want to get data from?\n[playlist] for your own\n[album] for an album\n[track] for a single song\n[quit] to quit: ')
+                ''' Checks to see if the dataframe is empty to add or append to the dataframe '''                
+                if (final_playlist.empty == True):
+                    final_playlist = album_tracks
+                else:
+                    final_playlist = final_playlist.append(album_tracks)
 
-    if (user_answer.lower() == 'album'):
-        user_answer = input('Input an album URL: ')
-        try:
-            album_tracks = get_album_tracks(user_answer)
-            if (final_playlist.empty == True):
-                final_playlist = album_tracks
-            else:
-                final_playlist = final_playlist.append(album_tracks)
-            user_answer = input('Want to add another playlist or album?(y/n) ')
-            if (user_answer.lower() == 'n'):
-                keepRunning = False
-                playlist_to_csv(final_playlist)
-        except TypeError:
-            print("Not a valid URL.")
+                ''' Checks in with user to add new tracks or create the csv '''                    
+                user_answer = input('Want to add another playlist or album?(y/n) ')
 
-    elif(user_answer.lower() == 'playlist'):
-        user_answer = input('Input a playlist URL: ')
-        try:
-            playlist_tracks = get_playlist_tracks(user_answer)
+                if (user_answer.lower() == 'n'):
+                    keepRunning = False
+                    playlist_to_csv(final_playlist)
+            except TypeError:
+                print("Not a valid URL.")
 
-            if (final_playlist.empty == True):
-                final_playlist = playlist_tracks
-            else:
-                final_playlist = final_playlist.append(playlist_tracks)
+        elif(user_answer.lower() == 'playlist'):
+            user_answer = input('Input a playlist URL: ')
+            try: # Exception handling if the user inputs an invalid URL 
+                playlist_tracks = get_playlist_tracks(user_answer)
 
-            user_answer = input('Want to add another playlist or album?(y/n) ')
+                ''' Checks to see if the dataframe is empty to add or append to the dataframe '''
+                if (final_playlist.empty == True):
+                    final_playlist = playlist_tracks
+                else:
+                    final_playlist = final_playlist.append(playlist_tracks)
 
-            if (user_answer.lower() == 'n'):
-                keepRunning = False
-                playlist_to_csv(final_playlist)
+                ''' Checks in with user to add new tracks or create the csv '''
+                ''' Checks in with user to add new tracks or create the csv '''
 
-        except TypeError:
-            print("Not a valid URL.")
+                user_answer = input('Want to add another playlist or album?(y/n) ')
 
-    elif(user_answer.lower() == 'track'):
-        user_answer = input('Input a track URL: ')
-        user_answer_uri = user_answer[(len(
-            'https://open.spotify.com/track/')):(len('https://open.spotify.com/track/') + 22)]
+                if (user_answer.lower() == 'n'):
+                    keepRunning = False
+                    playlist_to_csv(final_playlist)
 
-        try:
-            if (final_playlist.empty == True):
-                final_playlist = get_music_data([user_answer_uri])
-            else:
-                final_playlist = final_playlist.append(
-                    get_music_data([user_answer_uri]))
+            except TypeError:
+                print("Not a valid URL.")
 
-            user_answer = input('Want to add another playlist or album?(y/n) ')
+        elif(user_answer.lower() == 'track'):
+            user_answer = input('Input a track URL: ')
 
-            if (user_answer.lower() == 'n'):
-                keepRunning = False
-                playlist_to_csv(final_playlist)
+            user_answer_uri = user_answer[(len(
+                'https://open.spotify.com/track/')):(len('https://open.spotify.com/track/') + 22)] 
 
-        except TypeError:
-            print("Not a valid URL.")
+            try: # Exception handling if the user inputs an invalid URL 
+                ''' Checks to see if the dataframe is empty to add or append to the dataframe '''
+                if (final_playlist.empty == True):
+                    final_playlist = get_music_data([user_answer_uri])
+                else:
+                    final_playlist = final_playlist.append(
+                        get_music_data([user_answer_uri]))
 
-    elif(user_answer.lower() == 'quit'):
-        keepRunning = False
+                ''' Checks in with user to add new tracks or create the csv '''
+                user_answer = input('Want to add another playlist or album?(y/n) ')
+
+                if (user_answer.lower() == 'n'):
+                    keepRunning = False
+                    playlist_to_csv(final_playlist) # Creates csv file
+
+            except TypeError:
+                print("Not a valid URL.")
+
+        elif(user_answer.lower() == 'quit'):
+            keepRunning = False
+
+''' Main method to run the program '''
+if __name__ == "__main__":
+
+    ''' setting up '''
+    load_dotenv()
+
+    # authorization URL to gain access to the Spotify API
+    AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+    # POST to get Spotify access token 
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': os.getenv('CLIENT_ID'), # Spotify integration ID
+        'client_secret': os.getenv('CLIENT_SECRET'), # Spotify integration secret
+        "expires_in": 3600
+    })
+
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
+
+    # save the access token
+    access_token = auth_response_data['access_token']
+
+    # headers to request anything in the Spotify API 
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token)
+    }
+    
+    # base URL of all Spotify API endpoints
+    BASE_URL = 'https://api.spotify.com/v1/'
+
+    # run program
+    run()
